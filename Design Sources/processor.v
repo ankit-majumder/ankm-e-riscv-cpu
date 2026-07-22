b5fe293642ce4cf8329288a8b97de61ce7d26593
ankit-majumder <ankit.majumder.tx@gmail.com>
2026-07-22 12:47:05 -0500

    Began developing top level processor module, created intermediary adders, and pc mux, instantiated program counter, default next instruction pointer, instruction memory, instruction decoder, immediate generator, and JAL/Branch taken next instruction pointer

--- Design Sources/processor.v
+++ Design Sources/processor.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`include "defines.v"
 //////////////////////////////////////////////////////////////////////////////////
 // Company: N/A
 // Engineer: Ankit Majumder
@@ -19,8 +20,46 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+//adder for pc
+module adder(input[31:0] pc, val, output[31:0] result);
+    assign result = pc + val;
+endmodule
+
+//branch/jump select
+module pcmux(input[31:0] pcplus4, pcplusimm, aluresult, input[1:0] pc_select, output reg[31:0] pc_next);
+    always@(*) begin
+        case(pc_select)
+            `STANDARD: pc_next = pcplus4;
+            `JAL: pc_next = pcplusimm;
+            `JALR: pc_next = aluresult;
+        endcase
+    end        
+endmodule
 
 module processor(
-
+    input clk, reset
     );
+    
+    wire[31:0] pc_next, pc, instruction, pcstandard, pcjal, immediate;
+    
+    //Control Signals
+    wire[3:0] alu_op;
+    wire[2:0] imm_ctrl, branch_type, mem_ctrl;
+    wire[1:0] pc_ctrl, wb_ctrl;
+    wire rf_we, alu_ctrl1, alu_ctrl2, data_read, data_write;
+     
+    PC ip(.clk(clk), .reset(reset), .pc_next(pc_next), .pc(pc)); // program counter
+    adder pcplus4(.pc(pc), .val(32'd4), .result(pcstandard)); //default next instruction pointer
+    IMEM imem(.pc(pc), .instruction(instruction)); //instruction memory
+    //instruction decoder
+    intr_decode controller(.funct7(instruction[31]), .funct3(instruction[14:12]), .opcode(instruction[6:0]), .alu_op(alu_op),
+        .imm_ctrl(imm_ctrl), .branch_type(branch_type), .mem_ctrl(mem_ctrl), .pc_ctrl(pc_ctrl), .wb_ctrl(wb_ctrl), .rf_we(rf_we),
+        .alu_ctrl1(alu_ctrl1), .alu_ctrl2(alu_ctrl2), .data_read(data_read), .data_write(data_write));
+    
+    imm_generate(.imm_sel(imm_ctrl), .instruction(instruction), .immediate(immediate)); //immediate generator
+    adder pcplusimm(.pc(pc), .val(immediate), .result(pcjal)); //JAL/Branch taken next instruction pointer
+    
+    
+    
+    
 endmodule
